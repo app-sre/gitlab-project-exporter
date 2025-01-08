app-sre/gitlab-project-exporter

commit 272936f8c7ad7e2543e66092087ec5540e24dfb7

Move to uv
* Upgraded to python 3.12
* Refactored Dockerfile

APPSRE-11358

Signed-off-by: Rafa Porres Molina <rporresm@redhat.com>

Dockerfile
@@ -1,59 +1,48 @@
-FROM registry.access.redhat.com/ubi9/ubi-minimal:9.4@sha256:c0e70387664f30cd9cf2795b547e4a9a51002c44a4a86aa9335ab030134bf392 as base-python
+#
+# Base image with defaults for all stages
+FROM registry.access.redhat.com/ubi9/python-312@sha256:88ea2d10c741f169681102b46b16c66d20c94c3cc561edbb6444b0de3a1c81b3 AS base
+
 COPY LICENSE /licenses/LICENSE
-ENV PYTHONUNBUFFERED=1
 
-RUN microdnf upgrade -y && \
-    microdnf install -y \
-        shadow-utils \
-        python3.11 && \
-    microdnf clean all && \
-    update-alternatives --install /usr/bin/python3 python /usr/bin/python3.11 1 && \
-    ln -snf /usr/bin/python3.11 /usr/bin/python && \
-    python3 -m ensurepip
+#
+# Builder image
+#
+FROM base AS builder
+COPY --from=ghcr.io/astral-sh/uv:0.5.15@sha256:ea861f8e28b5c0e85ec14dc0f367d9d5cfa5b418024cc250219288d4fff591f1 /uv /bin/uv
 
-WORKDIR /code
-RUN useradd -u 5000 app && chown -R app:app /code
+ENV \
+    # use venv from ubi image
+    UV_PROJECT_ENVIRONMENT=$APP_ROOT \
+    # compile bytecode for faster startup
+    UV_COMPILE_BYTECODE="true" \
+    # disable uv cache. it doesn't make sense in a container
+    UV_NO_CACHE=true
 
-# ---- Interims image to get and build all the python dependencies ----
-FROM base-python as build-base-python
-ENV POETRY_HOME=/opt/poetry \
-    POETRY_VIRTUALENVS_CREATE=false \
-    PIP_DISABLE_PIP_VERSION_CHECK=on \
-    PATH=/opt/poetry/bin:$PATH
+COPY pyproject.toml uv.lock ./
+# Test lock file is up to date
+RUN uv lock --check
+# Install the project dependencies
+RUN uv sync --frozen --no-install-project --no-group dev
 
-# install poetry
-ARG POETRY_VERSION
-RUN POETRY_VERSION=$POETRY_VERSION python3 - < <(curl -sSL https://install.python-poetry.org) && poetry --version
+COPY README.md ./
+COPY gitlab_project_exporter ./gitlab_project_exporter
+RUN uv sync --frozen --no-group dev
 
-# required for poetry to install the dependencies
-COPY --chown=app:app pyproject.toml poetry.lock README.md /code/
-# the code
-COPY --chown=app:app gitlab_project_exporter /code/gitlab_project_exporter/
-COPY --chown=app:app app.sh /code/
+#
+# Test image
+#
+FROM builder AS test
 
-# install the python dependencies
-RUN poetry install --no-interaction --no-ansi --only main
+COPY Makefile ./
+RUN uv sync --frozen
 
-# ---- Test image ----
-FROM build-base-python as test
-# install the test dependencies
-RUN microdnf install -y make
-RUN poetry install --no-interaction --no-ansi
-# install the tests
-COPY --chown=app:app tests /code/tests/
-COPY --chown=app:app Makefile /code/
-USER app:app
+COPY tests ./tests
 RUN make test
 
-# ---- Bundle everything together in the final image ----
-FROM base-python
+#
+# Production image
+#
+FROM base AS prod
 EXPOSE 8080
-
-# get all the python dependencies from the build-python stage
-COPY --from=build-base-python /usr/local/lib/python3.11/site-packages/ /usr/local/lib/python3.11/site-packages/
-COPY --from=build-base-python /usr/local/lib64/python3.11/site-packages/ /usr/local/lib64/python3.11/site-packages/
-COPY --from=build-base-python /usr/local/bin/ /usr/local/bin/
-COPY --from=build-base-python /code/ /code/
-
-USER app:app
+COPY --from=builder /opt/app-root /opt/app-root
 CMD ["/code/app.sh"]

.tekton/gitlab-project-exporter-main-push.yaml
@@ -27,6 +27,8 @@ spec:
     value: Dockerfile
   - name: path-context
     value: .
+  - name: target-stage
+    value: prod
   pipelineRef:
     resolver: git
     params:

.tool-versions
@@ -1,3 +1,1 @@
-# this file is used by asdf to automatically select the correct tool versions
-# but it also serves as a reference for non-asdf users
-python 3.11.9
+uv

Makefile
@@ -1,25 +1,29 @@
-CODE_ROOT := gitlab_project_exporter
-BUILD_ARGS := POETRY_VERSION=1.8.3
 CONTAINER_ENGINE ?= $(shell which podman >/dev/null 2>&1 && echo podman || echo docker)
 IMAGE_NAME ?= gitlab-project-exporter
 IMAGE_TAG ?= $(shell git rev-parse --short=7 HEAD)
 
 format:
-	poetry run ruff check
-	poetry run ruff format
+	uv run ruff check
+	uv run ruff format
 .PHONY: format
 
 test:
-	poetry run ruff check --no-fix
-	poetry run ruff format --check
-	poetry run mypy
-	poetry run pytest -vv --cov=gitlab_project_exporter --cov-report=term-missing --cov-report xml
+	uv lock --locked
+	uv run ruff check --no-fix
+	uv run ruff format --check
+	uv run mypy
+	uv run pytest -vv --cov=gitlab_project_exporter --cov-report=term-missing --cov-report xml
 .PHONY: test
 
-container:
-	$(CONTAINER_ENGINE) build -t $(IMAGE_NAME):$(IMAGE_TAG) $(foreach arg,$(BUILD_ARGS),--build-arg $(arg)) .
-.PHONY: container
+build:
+	$(CONTAINER_ENGINE) build -t $(IMAGE_NAME):$(IMAGE_TAG) --target prod .
+	$(CONTAINER_ENGINE) tag $(IMAGE_NAME):$(IMAGE_TAG) $(IMAGE_NAME):latest
+.PHONY: build
 
 container-test:
 	$(CONTAINER_ENGINE) build --target test -t $(IMAGE_NAME):test-$(IMAGE_TAG) $(foreach arg,$(BUILD_ARGS),--build-arg $(arg)) .
 .PHONY: container-test
+
+.PHONY: dev-env
+dev-env:
+	uv sync

dev.sh
@@ -5,4 +5,4 @@ trap "rm -rf $PROMETHEUS_MULTIPROC_DIR" INT TERM EXIT
 export UVICORN_RELOAD="true"
 export LOG_LEVEL=DEBUG
 
-poetry run python gitlab_project_exporter/main.py
+uv run python gitlab_project_exporter/main.py

pyproject.toml
@@ -1,47 +1,54 @@
-[tool.poetry]
+[project]
 name = "gitlab-project-exporter"
-version = "0.1.0"
+version = "0.2.0"
 description = "Gitlab Project Prometheus exporter"
-authors = ["AppSRE <sd-app-sre@redhat.com>"]
-license = "Apache 2.0"
-readme = "README.md"
-packages = [{ include = "gitlab_project_exporter" }]
+authors = [{ name = "Red Hat App-SRE Team", email = "sd-app-sre@redhat.com" }]
+license = { text = "Apache 2.0" }
+requires-python = "~= 3.12.0"
+dependencies = [
+    "fastapi ~=0.115.6",
+    "python-gitlab ~=4.13.0",
+    "pydantic ~=2.9.2",
+    "pydantic-settings ~=2.0.3",
+    "prometheus-client ~=0.21.1",
+    "uvicorn ~=0.32.0",
+]
+
+[project.urls]
 homepage = "https://github.com/app-sre/gitlab-project-exporter"
 repository = "https://github.com/app-sre/gitlab-project-exporter"
 documentation = "https://github.com/app-sre/gitlab-project-exporter"
 
-[tool.poetry.dependencies]
-fastapi = "^0.115.4"
-python = "^3.11"
-python-gitlab = "^4.13.0"
-pydantic = "^2.9.2"
-pydantic-settings = "^2.0.3"
-prometheus-client  = "^0.21.0"
-uvicorn = "^0.32.0"
-
-[tool.poetry.group.dev.dependencies]
-httpx = "^0.27.2"
-mypy = "^1.13.0"
-pytest = "^8.3.3"
-pytest-cov = "^5.0.0"
-pytest-mock = "^3.14.0"
-responses = "^0.25.3"
-ruff = "0.7.1"
-uvicorn = { extras = ["standard"], version = "^0.32.0" }
+[dependency-groups]
+dev = [
+    "httpx ~=0.27.2",
+    "mypy ~=1.13.0",
+    "pytest ~=8.3.4",
+    "pytest-cov ~=6.0.0",
+    "pytest-mock ~=3.14.0",
+    "responses ~=0.25.3",
+    "ruff ~= 0.8.4",
+    "uvicorn[standard] ~= 0.32.0",
+]
 
 [build-system]
-requires = ["poetry-core"]
-build-backend = "poetry.core.masonry.api"
+requires = ["hatchling"]
+build-backend = "hatchling.build"
+
+[tool.hatch.build.targets.sdist]
+only-include = ["gitlab_project_exporter"]
+
+[tool.hatch.build.targets.wheel]
+only-include = ["gitlab_project_exporter"]
 
 # Ruff configuration
 [tool.ruff]
 line-length = 88
-target-version = 'py311'
-required-version = "0.7.1" # keep in sync with dev dependencies!
 src = ["gitlab_project_exporter"]
 extend-exclude = [
-    ".local", # used by poetry in local venv
-    ".cache", # used by poetry in local venv
+    ".local",
+    ".cache",
+    "tmp",
 ]
 fix = true
 

commit daee48b032704c7bafea6bda805a2ffca06aafac

Fix missing script in Dockerfile
bug introduced in 272936f

Signed-off-by: Rafa Porres Molina <rporresm@redhat.com>

Dockerfile
@@ -24,7 +24,7 @@ RUN uv lock --check
 # Install the project dependencies
 RUN uv sync --frozen --no-install-project --no-group dev
 
-COPY README.md ./
+COPY README.md app.sh ./
 COPY gitlab_project_exporter ./gitlab_project_exporter
 RUN uv sync --frozen --no-group dev
 
@@ -45,4 +45,4 @@ RUN make test
 FROM base AS prod
 EXPOSE 8080
 COPY --from=builder /opt/app-root /opt/app-root
-CMD ["/code/app.sh"]
+CMD ["/opt/app-root/src/app.sh"]